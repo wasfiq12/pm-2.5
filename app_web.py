c55adc0847031743e4bb70618a98869d07eb98be
wasfiq12 <118099719+wasfiq12@users.noreply.github.com>
2026-09-22 23:45:31 +0700

    Update app_web.py

--- app_web.py
+++ app_web.py
@@ -4,6 +4,7 @@ import folium
 import streamlit.components.v1 as components
 import pandas as pd
 import matplotlib.pyplot as plt
+from google.oauth2 import service_account
 
 st.set_page_config(page_title="Dashboard Kualitas Udara Kalteng", layout="wide")
 st.title("🛰️ Dashboard Prediksi PM2.5 & Risiko Paparan Populasi")
@@ -12,7 +13,19 @@ st.markdown("**Lokasi:** Kalimantan Tengah | **Pemodelan:** Random Forest Machin
 st.markdown("---")
 
 try:
-    ee.Initialize(project='wasfiq12')
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/earthengine",
+            "https://www.googleapis.com/auth/cloud-platform"
+        ]
+    )
+
+    ee.Initialize(
+        credentials=credentials,
+        project="wasfiq"
+    )
+
 except Exception as e:
     st.error(f"Earth Engine gagal diinisialisasi: {e}")
     st.stop()
